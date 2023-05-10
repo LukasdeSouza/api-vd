fab998f54ea656329cf3da65a1d72313e7ddb390
LukasdeSouza <lucasdesouzasilva112@gmail.com>
2023-05-10 17:38:23 -0300

    creating the page routes and creating app

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+require('dotenv').config()
+
 const express = require('express')
 const mongoose = require('mongoose')
+
+const app = express()
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+app.use(express.json())
